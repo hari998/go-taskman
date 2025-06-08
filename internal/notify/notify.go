5c72e18acca763e5949a54b4c77cfd5cef89f98d
Hariom Tiwari <heyhariom24@gmail.com>
2025-06-08 04:07:39 +0000

    suppresed notification for wsl environment

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -2,11 +2,17 @@ package notify
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gen2brain/beeep"
 )
 
 func Show(title, message string) {
+
+	if os.Getenv("WSL_DISTRO_NAME") != "" {
+		return
+	}
+
 	err := beeep.Notify(title, message, "icon.png")
 
 	if err != nil {
